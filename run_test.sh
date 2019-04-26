172f1d3999ef4aca358117baffc56d67cdc41402
xuchencheng <xuchencheng@g04.health>
2019-04-26 22:24:18 +0800

    modified run_test

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
-python main.py -r testdata/train0 testdata/train1 testdata/train2 testdata/train3 -s ./Model -e 400 -b 50
+python main.py -r testdata/train0 testdata/train1 testdata/train2 testdata/train3 -s ./Model #-e 400 -b 50
 python main.py -t testdata/test0 testdata/test1 testdata/test2 testdata/test3 -p ./Model/model.ckpt-0 -k 4
 python main.py -t snp_train snp_test -n True -a 1 -k 4 -p ./Model/model.ckpt-0
