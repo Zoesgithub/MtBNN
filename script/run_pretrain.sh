74ac2900337e27ae262310f44d141110ee702952
xcc <xucc19982@gmail.com>
2022-09-17 16:45:42 +0800

    update script

--- script/run_pretrain.sh
+++ script/run_pretrain.sh
@@ -1,8 +1,14 @@
 
+rm -r tasks/GM12878/bnn_pretrain_scale3
 python main.py -c tasks/GM12878/pretrain_bnn_scale3
+rm -r tasks/HepG2/bnn_pretrain_scale3
 python main.py -c tasks/HepG2/pretrain_bnn_scale3
+rm -r tasks/TCell/bnn_pretrain_scale3
 python main.py -c tasks/TCell/pretrain_bnn_scale3
 
+rm -r tasks/GM12878/dnn_pretrain
 python main.py -c tasks/GM12878/pretrain_dnn
+rm -r tasks/HepG2/dnn_pretrain
 python main.py -c tasks/HepG2/pretrain_dnn
+rm -r tasks/TCell/dnn_pretrain_scale3
 python main.py -c tasks/TCell/pretrain_dnn
