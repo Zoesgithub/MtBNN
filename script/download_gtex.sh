eabe69badb08258aaa6a91606bcea907eca00bb7
xcc <xucc19982@gmail.com>
2022-09-24 12:22:48 +0800

    rm download gtex

--- script/download_gtex.sh
+++
@@ -1,8 +0,0 @@
-cd data
-mkdir CaseStudy
-cd CaseStudy
-mkdir GTEx
-cd GTEx
-wget https://storage.googleapis.com/gtex_analysis_v7/single_tissue_eqtl_data/GTEx_Analysis_v7_eQTL.tar.gz
-tar -xzvf  GTEx_Analysis_v7_eQTL.tar.gz
-gzip -d GTEx_Analysis_v7_eQTL/*
